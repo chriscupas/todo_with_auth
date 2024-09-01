87b68bddc33a9d2bead204e595a825851fa52b11
Christopher Cupas <cupas.christopher@gmail.com>
2024-09-01 17:01:05 +0300

    missing

---
+++ app/Http/Controllers/TodoWithoutAuthController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequests;
+use App\Todo;
+
+class TodoWithoutAuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = Todo::all()->paginate(20);
+
+        return view('todos.index', compact('todos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('todos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(TodoRequests $request)
+    {
+        $data = $request->only(array_keys($request->rules()));
+
+        Todo::create($data);
+
+        return redirect('todos');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        return $this->edit($todo);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Todo $todo)
+    {
+        return view('todos.edit')->with('todo', $todo);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(TodoRequests $request, Todo $todo)
+    {
+        $data = $request->only(array_keys($request->rules()));
+        $data['status'] = false;
+        $todo->fill($data);
+
+        $todo->save();
+        return redirect('todos');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return redirect('todos');
+    }
+}
